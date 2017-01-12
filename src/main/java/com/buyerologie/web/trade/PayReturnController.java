240505c16bf65023d8e91c8d7ef283695f58c7fb
Sleepwalker <yy993103@163.com>
2017-01-12 18:40:51 +0800

    对接会员

--- src/main/java/com/buyerologie/web/trade/PayReturnController.java
+++ src/main/java/com/buyerologie/web/trade/PayReturnController.java
@@ -3,34 +3,38 @@ package com.buyerologie.web.trade;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.buyerologie.trade.TradeService;
-import com.buyerologie.trade.pay.exception.PayException;
+import com.buyerologie.trade.exception.TradeException;
+import com.buyerologie.user.UserService;
+import com.buyerologie.user.exception.UserException;
+import com.buyerologie.vip.exception.VipException;
 
 @Controller
 public class PayReturnController {
 
     @Resource
-    private TradeService        tradeService;
+    private UserService  userService;
 
-    private static final Logger logger = Logger.getLogger(PayReturnController.class);
+    @Resource
+    private TradeService alipayTradeService;
 
-    @RequestMapping(value = "/pay_return", method = { RequestMethod.GET, RequestMethod.POST })
-    public String payReturn(HttpServletRequest request) throws PayException {
-
-        tradeService.payReturn(request);
+    @RequestMapping(value = "/alipay/pay_return", method = { RequestMethod.GET, RequestMethod.POST })
+    public String payReturn(HttpServletRequest request) throws TradeException, UserException,
+                                                       VipException {
+        alipayTradeService.payReturn(request);
         return "redirect:/trade/paysuccess";
     }
 
     @ResponseBody
-    @RequestMapping(value = "/pay_notify", method = { RequestMethod.GET, RequestMethod.POST })
-    public String payNotify(HttpServletRequest request) throws PayException {
-        tradeService.payReturn(request);
+    @RequestMapping(value = "/alipay/pay_notify", method = { RequestMethod.GET, RequestMethod.POST })
+    public String payNotify(HttpServletRequest request) throws TradeException, UserException,
+                                                       VipException {
+        alipayTradeService.payReturn(request);
         return "success";
     }
 }
